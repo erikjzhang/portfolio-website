2ac514049aa84f610185d11dbd2952dadefdaa45
Your Name <you@example.com>
2025-02-03 13:32:02 -0500

    Updated repository

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,8 +1,8 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import { TypeAnimation } from "react-type-animation";
-import { motion } from "framer-motion";
+import TypeAnimation from "react-type-animation";
+import motion from "framer-motion";
 
 const HeroSection = () => {
   return (
